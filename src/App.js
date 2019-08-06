38364b3237c7e496e41ba355bddff773642451c6
Zachary Graham <zakgraham@gmail.com>
2019-08-05 20:22:45 -0700

    add material

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 function App() {
+  console.log('hello');
+  const classes = useStyles();
+
   return (
     <div className="App">
 
-    4th wall youtube
-    https://www.youtube.com/channel/UCrMRs4dZmAAmKkbeLalixAA
-
+      <Button variant="contained" className={classes.button}>
+        Default
+      </Button>
+      
+  
     </div>
   );
 }
