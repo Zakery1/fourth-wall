ecdf46043186e4dd1c055821c88e92655d0dd47e
Zachary Graham <zakgraham@gmail.com>
2019-08-05 21:20:19 -0700

    add links to youtube and itunes

--- src/App.js
+++ src/App.js
@@ -20,8 +20,20 @@ function App() {
   return (
     <div className="App">
 
-      <Button variant="contained" className={classes.button}>
-        Default
+      <h1>Want to watch fourth wall?
+        <br/>
+      </h1>
+
+      {/* https://podcasts.apple.com/us/podcast/the-fourth-wall-podcast/id1464608975 */}
+
+      <Button  color="default" target="_blank" href="https://podcasts.apple.com/us/podcast/the-fourth-wall-podcast/id1464608975" variant="contained" className={classes.button}>
+        Apple Podcasts
+      </Button>
+      <br/>
+
+    
+      <Button  color="secondary" target="_blank" href="https://www.youtube.com/channel/UCrMRs4dZmAAmKkbeLalixAA" variant="contained" className={classes.button}>
+        Youtube
       </Button>
       
   
