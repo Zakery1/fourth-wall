bb0a9c4a94180bd33293361edcad47284b485bc4
Zachary Graham <zakgraham@gmail.com>
2020-06-28 20:18:18 -0700

    remove unused variables

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Modal from './modal/Modal';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import { useTheme } from '@material-ui/styles';
-import CardMedia from '@material-ui/core/CardMedia';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Fab from '@material-ui/core/Fab';
@@ -106,7 +103,6 @@ const useStyles = makeStyles(theme => ({
 function App() {
   console.log('hello');
   const classes = useStyles();
-  const theme = useTheme();
   return (
     <div className="App">
 
