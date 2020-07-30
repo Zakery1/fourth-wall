70b6f45d4ccaee99a9e033dcd91534b58850a902
Zachary Graham <zakgraham@gmail.com>
2020-07-29 22:56:10 -0700

    map out podcast links

--- src/App.js
+++ src/App.js
@@ -1,187 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import Modal from './modal/Modal';
+import Modal from "./modal/Modal";
 
-import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
+import podcastPlatforms from "./resource/podcastPlatforms";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Fab from "@material-ui/core/Fab";
+import Button from "@material-ui/core/Button";
+import { Popover } from "@material-ui/core";
 
-
-const useStyles = makeStyles(theme => ({
-
-  apple: {
-    border: 'solid #C463EB 2px',
-    color: '#C463EB',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  youtube: {
-    border: 'solid #FF0000 2px',
-    color: '#FF0000',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  google: {
-    border: 'solid #FF5722 2px',
-    color: '#FF5722',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  spotify: {
-    border: 'solid #1BD75F 2px',
-    color: '#1BD75F ',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  podcastRepublic: {
-    border: 'solid #5483DF 2px',
-    color: '#5483DF',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  podcastAddict: {
-    border: 'solid #F3873C 2px',
-    color: '#F3873C',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  iHeart: {
-    border: 'solid #C92325 2px',
-    color: '#C92325',
-    marginRight: theme.spacing(1),
-    marginTop: theme.spacing(1),
-    fontSize: '12px',
-    width: '180px',
-    background: '#f1f4f6',
-    height: '55px'
-  },
-
-  card: {
-    height: '200px'
-  },
-
-  root: {
-    padding: theme.spacing(1, 1),
-    color: 'rgb(65, 65, 65)'
-  }
-}));
 
 
 function App() {
-  const classes = useStyles();
+
+  const platformLinks = podcastPlatforms.map((platform) => {
+    return (
+      <Button target="_blank" href={platform.url} key={platform.id} style={{background: platform.hex}} variant="contained" color="primary">
+        {platform.name}
+        <i className="material-icons">arrow_forward</i>
+      </Button>
+    );
+  });
+
   return (
     <div className="App">
-
       <div className="zg-top">
-
         <div className="card-container">
-          <Modal/>
+          <Modal />
         </div>
 
         <div className="paper-container">
-          <Paper className={classes.root}>
+          <Paper >
             <Typography component="p">
               Let's listen to Stage 18 on...
-          </Typography>
+            </Typography>
           </Paper>
         </div>
-
       </div>
-      
-      <div className="link-container">
 
-        <Fab target="_blank" href="https://podcasts.apple.com/us/podcast/the-fourth-wall-podcast/id1464608975" variant="extended" className={classes.apple}>
-          Listen on Apple <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-        <br />
-
-        <Fab target="_blank" href="https://www.youtube.com/channel/UCrMRs4dZmAAmKkbeLalixAA"  variant="extended" className={classes.youtube}>
-          Listen on Youtube <i className="material-icons">
-            arrow_forward
-                </i>
-        </Fab>
-        <br />
-
-        <Fab target="_blank" href="https://play.google.com/music/m/Isq5occ2n75eytqi3e44xes5ouy?t=The_Fourth_Wall_Podcast" variant="extended" className={classes.google}>
-          Listen on Google
-          <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-        <br />
-
-        <Fab target="_blank" href="https://open.spotify.com/show/75978jAwNVsjI2H6W4Gyux" variant="extended" className={classes.spotify}>
-          Listen on Spotify
-          <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-
-        <br />
-
-        <Fab target="_blank" href="https://www.podcastrepublic.net/podcast/1464608975" variant="extended" className={classes.podcastRepublic}>
-          Listen on Podcast Republic
-          <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-
-        <br />
-
-        <Fab target="_blank" href="http://podplayer.net/?podId=2422284&fbclid=IwAR1xK-By7B64duf_tnlOECVhOfwUBXolnNtnvKd_DrDjU_PkHXV8vJ0X5ZU" variant="extended" className={classes.podcastAddict}>
-          Listen on Podcast Addict
-          <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-
-        <br />
-
-        <Fab target="_blank" href="https://www.iheart.com/podcast/966-the-fourth-wall-podcast-45924242/" variant="extended" className={classes.iHeart}>
-          Listen on iHeart Radio
-          <i className="material-icons">
-            arrow_forward
-                </i>
-          </Fab>
-        <br/>
-        <br/>
-        <br/>
-        <br />
-      </div>
+      {platformLinks}
     </div>
   );
 }
---
+++ src/resource/podcastPlatforms.js
@@ -0,0 +1,49 @@
+const podcastPlatforms = [
+  {
+    id: 1,
+    name: "Apple",
+    url:
+      "https://podcasts.apple.com/us/podcast/the-fourth-wall-podcast/id1464608975",
+    hex: "#C463EB",
+  },
+  {
+    id: 2,
+    name: "Youtube",
+    url: "https://www.youtube.com/channel/UCrMRs4dZmAAmKkbeLalixAA",
+    hex: "#FF0000",
+  },
+  {
+    id: 3,
+    name: "Google Play",
+    url:
+      "https://play.google.com/music/m/Isq5occ2n75eytqi3e44xes5ouy?t=The_Fourth_Wall_Podcast",
+    hex: "#FF5722",
+  },
+  {
+    id: 4,
+    name: "Spotify",
+    url: "https://open.spotify.com/show/75978jAwNVsjI2H6W4Gyux",
+    hex: "#1BD75F",
+  },
+  {
+    id: 5,
+    name: "Podcast Republic",
+    url: "https://www.podcastrepublic.net/podcast/1464608975",
+    hex: "#5483DF",
+  },
+  {
+    id: 6,
+    name: "Podcast Addict",
+    url:
+      "http://podplayer.net/?podId=2422284&fbclid=IwAR1xK-By7B64duf_tnlOECVhOfwUBXolnNtnvKd_DrDjU_PkHXV8vJ0X5ZU",
+    hex: "#F3873C",
+  },
+  {
+    id: 7,
+    name: "IHeart",
+    url: "https://www.iheart.com/podcast/966-the-fourth-wall-podcast-45924242/",
+    hex: "#C92325",
+  },
+];
+
+export default podcastPlatforms;
