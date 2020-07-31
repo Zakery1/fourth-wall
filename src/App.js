6656e45bb314a30b9f99bf6084924b01827a8e4d
Zachary Graham <zakgraham@gmail.com>
2020-07-30 21:28:21 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import React from "react";
 import Modal from "./modal/Modal";
 
 import podcastPlatforms from "./resource/podcastPlatforms";
-import "./App.css";
-import { makeStyles } from "@material-ui/core/styles";
+import "./App.scss";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Fab from "@material-ui/core/Fab";
 import Button from "@material-ui/core/Button";
 import { Popover } from "@material-ui/core";
 
-
-
 function App() {
-
   const platformLinks = podcastPlatforms.map((platform) => {
     return (
-      <Button target="_blank" href={platform.url} key={platform.id} style={{background: platform.hex}} variant="contained" color="primary">
+      <Button
+        target="_blank"
+        href={platform.url}
+        key={platform.id}
+        style={{ background: platform.hex }}
+        variant="contained"
+        color="primary"
+      >
         {platform.name}
         <i className="material-icons">arrow_forward</i>
       </Button>
@@ -27,12 +30,12 @@ function App() {
   return (
     <div className="App">
       <div className="zg-top">
-        <div className="card-container">
+        {/* <div className="card-container">
           <Modal />
-        </div>
+        </div> */}
 
         <div className="paper-container">
-          <Paper >
+          <Paper>
             <Typography component="p">
               Let's listen to Stage 18 on...
             </Typography>
