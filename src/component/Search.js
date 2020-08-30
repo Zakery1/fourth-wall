1e6a950b05e2f561a58659c29e80e869b9de4267
Zachary Graham <zakgraham@gmail.com>
2020-08-29 21:30:03 -0700

    add front end for search

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,36 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Search.scss";
 
 function Search() {
   const [data, setData] = useState([]);
-  // const [search]
+  const [noResultsMessage, setNoResultsMessage] = useState("");
 
-  useEffect(() => {}, []);
+  const searchByMovie = (event) => {
+    if (event.key !== "Enter") {
+      return;
+    }
 
-  const searchByMovie = (searchInput) => {
+    const searchInput = event.target.value;
+
     const fetchData = async () => {
+      if (searchInput.trim().length === 0) {
+        setData([]);
+        setNoResultsMessage("");
+        return;
+      }
       const response = await axios.get(
-        `https://limitless-lowlands-38782.herokuapp.com/api/movie/search?movie=${searchInput}`
+        `http://localhost:8080/api/movie/search?movie=${searchInput}` ||
+          `${process.env.API_URL}/api/movie/search?movie=${searchInput}`
       );
+      if (response.data.length === 0) {
+        setData([]);
+        setNoResultsMessage(
+          "Sorry, we have not discussed a movie matching your search"
+        );
+        return;
+      }
+      setNoResultsMessage("");
       setData(response.data);
     };
     fetchData();
   };
 
-
-
   const relevantMovie = data.map((movie) => {
-
-    const searchedEpisodes = movie.episodes.map(episode => {
-      return <div key={episode._id}>
-        Season: {episode.seasonNumber} <br/>
-        Episode Title: {episode.episodeName}
-      </div>
+    const searchedEpisodes = movie.episodes.map((episode) => {
+      return (
+        <div key={episode._id}>
+          Season: {episode.seasonNumber} <br />
+          Episode Title: {episode.episodeName}
+        </div>
+      );
     });
-
     return (
-      <div key={movie._id}> We discuss {movie.name} in these episodes:{searchedEpisodes} <br/></div>
+      <div key={movie._id}>
+        We discuss {movie.name} in these episodes:{searchedEpisodes} <br />
+      </div>
     );
   });
 
@@ -39,8 +57,9 @@ function Search() {
   return (
     <div className="zg-search-container">
       Search <br />
-      <input onChange={(event) => searchByMovie(event.target.value)} /> <br />
+      <input onKeyDown={(event) => searchByMovie(event)} /> <br />
       {relevantMovie}
+      {noResultsMessage}
     </div>
   );
 }
